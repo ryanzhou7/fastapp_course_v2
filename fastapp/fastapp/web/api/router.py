4c55ea69516247f611ded7d52e7da224e52b1ad2
Ryan <5668783+ryanzhou7@users.noreply.github.com>
2024-11-12 21:51:43 -0800

    wip

--- fastapp/fastapp/web/api/router.py
+++ fastapp/fastapp/web/api/router.py
@@ -1,6 +1,7 @@
 from fastapi.routing import APIRouter
 
-from fastapp.web.api import monitoring
+from fastapp.web.api import echo, monitoring
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
+api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
